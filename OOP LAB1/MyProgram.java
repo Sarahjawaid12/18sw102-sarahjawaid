87415df552d7301cd9919eb59b5c4f68281d16de
Sarahjawaid12 <49403823+Sarahjawaid12@users.noreply.github.com>
2019-04-21 20:18:38 +0500

    Add files via upload

---
+++ OOP LAB1/MyProgram.java
@@ -0,0 +1,18 @@
+class MyProgram
+{
+  
+     public static void main(String args[])
+{
+      String name="Sarah";
+      double marks=90;
+      float percentage=95.5f;
+      char grade='A';
+      boolean status= true;
+      char p='%'; 
+      System.out.println("Name:" +name);
+      System.out.println("TotalMarks:" +marks);
+      System.out.println("Percentage:" +percentage);
+      System.out.println("Grade:" +grade);
+      System.out.println("Status:Pass:" +status);
+       }
+                            }
---
+++ OOP LAB1/SecondProgram.java
@@ -0,0 +1,11 @@
+class SecondProgram{
+      public static void main(String args[])
+    {
+          double a=50.26;
+          int b= (int)a;
+          float c=(float)(a-b);
+          System.out.println(a);
+          System.out.println(b);
+          System.out.println(c);
+   } 
+          }
